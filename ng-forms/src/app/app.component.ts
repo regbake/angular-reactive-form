2cdbff962de2ceb8863783a592ff71f07d0301b2
regbake <regbake@gmail.com>
2018-04-16 18:34:23 -0700

    initial commit

---
+++ ng-forms/src/app/app.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  rForm : FormGroup;
+  post: any;
+  description: string = '';
+  name: string = '';
+
+  constructor(private fb: FormBuilder) {
+    this.rForm = fb.group({
+      'name' : [null, Validators.required],
+      'description': [null, Validators.compose([Validators.required, Validators.minLength(30), Validators.maxLength(500)]),
+      'validate': ''
+    })
+  }
+
+  addPost(post) {
+    this.description = post.description;
+    this.name = post.name;
+  }
+
+}
