8fdd13ac604458180143942fb7dda8a7a44fc5c3
regbake <regbake@gmail.com>
2018-04-16 20:30:56 -0700

    fixed the min character issue

--- ng-forms/src/app/app.component.ts
+++ ng-forms/src/app/app.component.ts
@@ -15,7 +15,7 @@ export class AppComponent {
   constructor(private fb: FormBuilder) {
     this.rForm = fb.group({
       'name' : [null, Validators.required],
-      'description': [null, Validators.compose([Validators.required, Validators.minLength(30), Validators.maxLength(500)]),
+      'description': [null, Validators.compose([Validators.required, Validators.minLength(30), Validators.maxLength(500)])],
       'validate': ''
     })
   }
